a0e892137823e5ecab5f75409eef419304a3c608
naimkim <naimkim2000@naver.com>
2020-10-08 16:39:45 +0900

    04_combination

--- main.c
+++ main.c
@@ -3,20 +3,51 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-int square(int a)
+int factorial(int n)
 {
-	return (a*a);	
+	
+	int i;
+	int res = 1;
+	
+	for (i=1; i<=n; i++)
+		res = res*i;
+	
+	return res;
+}
+
+int combination(int n, int r)
+{
+	int n1, n2, n3;
+	
+	n1 = factorial(n);
+	n2 = factorial(n-r);
+	n3 = factorial(r);
+	
+		
+	return (n1/n2/n3);	
 }
 
 
+int get_integer()
+{
+	int x; 
+	printf("분모 분자 입력 : ");
+	scanf("%i", &x);
+	return x;
+}
 int main(int argc, char *argv[]) {
 	
-	int a = 2;
+	int n;
+	int r;
+	int result;
 	
-	a = square(a);
-	printf("a=%i\n", a);
+	n = get_integer();
 	
+	r = get_integer();
+	
+	result = combination(n,r);
+	
+	printf("combination result is %i\n", result);
 	
 	return 0;
 }
